f06eedb139d186fb017e226bf7a63885d641e893
jeremie-codes <125778647+jeremie-codes@users.noreply.github.com>
2023-12-08 15:08:53 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const connection = mysql.createPool({
+  host     : 'localhost', // Your connection adress (localhost).
+  user     : 'root',     // Your database's username.
+  password : 'root',        // Your database's password.
+  database : 'sme'   // Your database's name.
+});
+
+// Starting our app.
+const app = express();
+
+// Creating a GET route that returns data from the 'users' table.
+app.get('/users', function (req, res) {
+
+  // res.header('Access-Control-Allow-Origin', "*");
+  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  // res.header('Access-Control-Allow-Headers', 'Content-Type');
+  // next();
+    
+  // Connecting to the database.
+  connection.getConnection(function (err, connection) {
+
+    // Executing the MySQL query (select all data from the 'users' table).
+    connection.query('SELECT * FROM utilisateur', function (error, results, fields) {
+      // If some error occurs, we throw an error.
+      if (error) throw error;
+
+      // Getting the 'response' from the database and sending it to our route. This is were the data is.
+      res.send(results)
+    });
+  });
+});
+
+// Starting our server.
+app.listen(3000, () => {
+ console.log('Go to http://127.0.0.1:3000/users so you can see the data.');
+});
